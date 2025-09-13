679c918e5aa5b513ee6ef63852776802ccc9cd86
Uverlanio Mauricio <uverlanio.pereira@globant.com>
2025-09-13 14:02:53 -0300

    Primeiro commit do projeto PacienteApp

---
+++ src/main/java/com/pereira/jaala/prontuarios/ProntuariosApplication.java
@@ -0,0 +1,13 @@
+package com.pereira.jaala.prontuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProntuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProntuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pereira/jaala/prontuarios/ProntuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pereira.jaala.prontuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProntuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
